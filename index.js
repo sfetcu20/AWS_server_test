a6c631e7a556d55acbf588519c19ff6af1313a01
sfetcu20 <sfetcualexandra19@stud.ase.ro>
2022-10-10 17:19:35 +0300

    response structure changed

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
+const bodyparser = require("body-parser");
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
-const port = 8080;
+app.use(bodyparser.json());
+app.use(cors());
+
+const port = 8090;
 
 app.get("/forgotPassword", (req, res) => {
-  res.json({ data: "Forgot password template" });
+  console.log(req);
+  res.json({ content: "Forgot password template", subiect: "Forgot Password" });
 });
 app.get("/resetPassword", (req, res) => {
-  res.json({ data: "Reset password template" });
+  res.json({ content: "Reset password template", subiect: "Reset Password" });
 });
 app.get("/saveOffer", (req, res) => {
-  res.json({ data: "Save offer template" });
+  res.json({ content: "Save offer template", subiect: "Save offer" });
 });
 app.get("/renewOffer", (req, res) => {
-  res.json({ data: "Renew offer template" });
+  res.json({ content: "Renew Offer template", subiect: "Renew Offer" });
 });
 app.get("/saveInsurance", (req, res) => {
-  res.json({ data: "Save Insurance template" });
+  res.json({
+    content: "Save Insurance template",
+    subiect: "Save Insurance",
+  });
 });
 app.get("/transferObject", (req, res) => {
-  res.json({ data: "Transfer object template" });
+  res.json({ content: "Transfer object template", subiect: "Transfer object" });
 });
 
 app.listen(port, () => {
